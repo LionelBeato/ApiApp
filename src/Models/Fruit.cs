f1c22ff8f9fd1c57aef018366fd711c31bb3c1b6
Lionel Beato <lionel.beato@gmail.com>
2022-03-07 14:25:36 -0500

    remove comments

--- src/Models/Fruit.cs
+++ src/Models/Fruit.cs
@@ -3,7 +3,4 @@ namespace ApiApp.Models;
 public record Fruit (string Name)
 {
     public int FruitId { get; init; }
-    // public string Name { get; set; }
-    // public List<Yoshi> Yoshis { get; set; }
-
 }
